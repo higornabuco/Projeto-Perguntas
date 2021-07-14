b868f7687fc78e6e4d0e2c58f430cfcf3701fe7e
Higor Nabuco <higornabuco@gmail.com>
2021-07-14 16:46:52 -0300

    Enviando projeto

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é seu animal favorita?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+                child: Text('Resposta 2'),
+                onPressed: () {
+                  print('Resposta 2 foi selecionada!');
+                }),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: () => print('Resposta 3!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
