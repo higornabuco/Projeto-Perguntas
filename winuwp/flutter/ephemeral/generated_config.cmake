c36b18bac4b5d4e43ca5172f795ea1245812ae23
Higor Nabuco <higornabuco@gmail.com>
2021-07-15 15:24:12 -0300

    Atualizando projeto

---
+++ winuwp/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\higor\\Documents\\Projetos\\curso_flutter\\projeto_perguntas" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\higor\\Documents\\Projetos\\curso_flutter\\projeto_perguntas"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\higor\\Documents\\Projetos\\curso_flutter\\projeto_perguntas\\winuwp\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\higor\\Documents\\Projetos\\curso_flutter\\projeto_perguntas"
+  "FLUTTER_TARGET=C:\\Users\\higor\\Documents\\Projetos\\curso_flutter\\projeto_perguntas\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\higor\\Documents\\Projetos\\curso_flutter\\projeto_perguntas\\.dart_tool\\package_config.json"
+)
